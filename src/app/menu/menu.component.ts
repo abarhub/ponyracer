dfd94e4af64a3f74ee7e14663d49eb672289d88c
abarhub <abarhub@gmail.com>
2024-02-10 20:54:13 +0100

    ajout du menu

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pr-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {}
