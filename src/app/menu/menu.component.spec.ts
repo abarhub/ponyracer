f6a851ca0aa0e0632786be891d4b6fd3e750aabb
abarhub <abarhub@gmail.com>
2024-02-11 08:20:44 +0100

    exercice 2

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,22 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { MenuComponent } from './menu.component';
 
 describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+  beforeEach(() => TestBed.configureTestingModule({}));
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MenuComponent]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(MenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should have a `navbarCollapsed` field', () => {
+    const fixture = TestBed.createComponent(MenuComponent);
+    expect(fixture.componentInstance.navbarCollapsed)
+      .withContext(
+        'Check that `navbarCollapsed` is initialized with `true`. Maybe you forgot to declare `navbarCollapsed` in your component.'
+      )
+      .toBe(true);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have a `toggleNavbar` method', () => {
+    const fixture = TestBed.createComponent(MenuComponent);
+    expect(fixture.componentInstance.toggleNavbar).withContext('Maybe you forgot to declare a `toggleNavbar()` method').not.toBeNull();
+
+    fixture.componentInstance.toggleNavbar();
+
+    expect(fixture.componentInstance.navbarCollapsed)
+      .withContext('`toggleNavbar()` should change `navbarCollapsed` from `true` to `false`')
+      .toBe(false);
+
+    fixture.componentInstance.toggleNavbar();
+
+    expect(fixture.componentInstance.navbarCollapsed)
+      .withContext('`toggleNavbar()` should change `navbarCollapsed` from false to true`')
+      .toBe(true);
+  });
+
+  it('should toggle the class on click', () => {
+    const fixture = TestBed.createComponent(MenuComponent);
+    const element = fixture.nativeElement;
+
+    fixture.detectChanges();
+
+    const navbarCollapsed = element.querySelector('#navbar');
+    expect(navbarCollapsed).withContext('No element with the id `#navbar`').not.toBeNull();
+    expect(navbarCollapsed.classList)
+      .withContext('The element with the id `#navbar` should have the class `collapse`')
+      .toContain('collapse');
+
+    const button = element.querySelector('button');
+    expect(button).withContext('No `button` element to collapse the menu').not.toBeNull();
+    button.dispatchEvent(new Event('click'));
+
+    fixture.detectChanges();
+
+    const navbar = element.querySelector('#navbar');
+    expect(navbar.classList)
+      .withContext('The element with the id `#navbar` should have not the class `collapse` after a click')
+      .not.toContain('collapse');
   });
 });
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {}
+export class MenuComponent {
+  navbarCollapsed = true;
+
+  toggleNavbar() {
+    this.navbarCollapsed = !this.navbarCollapsed;
+  }
+}
