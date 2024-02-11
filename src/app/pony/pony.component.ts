69d162de625d9cdf9c5ec30e2b5b5b94f3fa513c
abarhub <abarhub@gmail.com>
2024-02-11 10:24:40 +0100

    réalisation exercice 5

---
+++ src/app/pony/pony.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PonyModel } from '../models/pony.model';
+
+@Component({
+  selector: 'pr-pony',
+  standalone: true,
+  imports: [],
+  templateUrl: './pony.component.html',
+  styleUrl: './pony.component.css'
+})
+export class PonyComponent {
+  @Input({ required: true })
+  ponyModel!: PonyModel;
+
+  @Output()
+  ponyClicked = new EventEmitter<PonyModel>();
+
+  clicked() {
+    this.ponyClicked.emit(this.ponyModel);
+  }
+}
